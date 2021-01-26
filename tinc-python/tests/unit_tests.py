ff9f8e133cb6b5b5b265736a641bfe801b674b82
Andres Cabrera <mantaraya36@gmail.com>
2021-01-25 17:10:24 -0800

    Added initial unit test file

---
+++ tinc-python/tests/unit_tests.py
@@ -0,0 +1,34 @@
+import unittest
+
+import sys
+sys.path.append('../')
+from parameter import *
+
+external_value = 0
+def callback(value):
+    global external_value
+    external_value = value
+
+
+class ParameterTest(unittest.TestCase):
+
+    def test_parameter(self):
+        global external_value
+        p = Parameter("name", "group", -1, 1, 0.5)
+
+        self.assertEqual(p.id, "name")
+        self.assertEqual(p.group, "group")
+        self.assertEqual(p.minimum, -1)
+        self.assertEqual(p.maximum, 1)
+        self.assertEqual(p.value, 0.5)
+
+        p.value = 0.8
+        self.assertEqual(p.value, 0.8)
+
+        p.register_callback(callback)
+        p.value = 0.4
+
+        self.assertEqual(external_value, 0.4)
+
+if __name__ == '__main__': 
+    unittest.main()
