9e92b0d7e65a8d669dd8db48ba643f097b56c2fa
Andres Cabrera <mantaraya36@gmail.com>
2021-08-10 14:45:58 -0700

    Bump version to 0.9.25

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ print(setuptools.find_packages())
 
 setuptools.setup(
     name="tinc", 
-    version="0.9.24",
+    version="0.9.25",
     author="Andres Cabrera",
     author_email="acabrera@ucsb.edu",
     description="TINC python module",
