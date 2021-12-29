bac7754e0a5525c4745a96d06185fd3ee8d8af13
Andres Cabrera <mantaraya36@gmail.com>
2021-12-29 11:56:26 -0800

    Bumped version to 0.9.51

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ print(setuptools.find_packages())
 
 setuptools.setup(
     name="tinc", 
-    version="0.9.50",
+    version="0.9.51",
     author="Andres Cabrera",
     author_email="mantaraya36@gmail.com",
     description="TINC python module",
